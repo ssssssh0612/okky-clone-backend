2886bbbf17e26414fafd1697f7eba8a5c78544ac
sibjagun <sibjagun99@kyungmin.ac.kr>
2026-02-24 00:28:37 +0900

    feat(auth): 이메일 로그인 인증용 UserDetailsService 및 Principal 구현
    정규화된 이메일 기준으로 사용자를 조회하는 MemberUserDetailsService를 추가한다
    UserDetails 구현체인 MemberPrincipal를 추가한다
    인증 사용자 조회를 위해 MemberRepository.findByEmail(Optional)메서드를 추가한다

--- src/main/java/io/github/sanghyeon/boardservice/domain/member/repository/MemberRepository.java
+++ src/main/java/io/github/sanghyeon/boardservice/domain/member/repository/MemberRepository.java
@@ -1,13 +1,15 @@
 package io.github.sanghyeon.boardservice.domain.member.repository;
 
 import io.github.sanghyeon.boardservice.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 
+    boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
 }
